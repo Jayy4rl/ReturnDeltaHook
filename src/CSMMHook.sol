d61054b61d2a1152ae179b4c22c31b38c507619e
Valerie <122825327+Jayy4rl@users.noreply.github.com>
2026-05-21 09:11:59 +0000

    feat: Constant Sum Market Maker implemented

--- src/CSMMHook.sol
+++ src/CSMMHook.sol
@@ -9,8 +9,10 @@ import {BeforeSwapDelta, toBeforeSwapDelta} from "v4-core/types/BeforeSwapDelta.
 import {Currency} from "v4-core/types/Currency.sol";
 import {PoolKey} from "v4-core/types/PoolKey.sol";
 import {SwapParams} from "v4-core/types/PoolOperation.sol";
+import {IUnlockCallback} from "v4-core/interfaces/callback/IUnlockCallback.sol";
+import {IERC20Minimal} from "v4-core/interfaces/external/IERC20Minimal.sol";
 
-contract CSMMHook is BaseHook {
+contract CSMMHook is BaseHook, IUnlockCallback {
     constructor(IPoolManager _manager) BaseHook(_manager) {}
 
     function getHookPermissions() public pure override returns (Hooks.Permissions memory) {
@@ -48,5 +50,31 @@ contract CSMMHook is BaseHook {
         poolManager.mint(address(this), inputCurrency.toId(), absAmount);
         poolManager.burn(address(this), outputCurrency.toId(), absAmount);
         return (this.beforeSwap.selector, resolvedDelta, 0);
+    }
+
+    function addLiquidity(PoolKey calldata key, uint256 amount0, uint256 amount1) public {
+        bytes memory data = abi.encode(key, amount0, amount1, msg.sender);
+        poolManager.unlock(data);
+    }
+
+    function unlockCallback(bytes calldata data) external override returns (bytes memory) {
+
+        require(msg.sender == address(poolManager), "only poolManager");
+        (PoolKey memory key, uint256 amount0, uint256 amount1, address sender) =
+            abi.decode(data, (PoolKey, uint256, uint256, address));
+        if (amount0 > 0) {
+            poolManager.sync(key.currency0);
+            IERC20Minimal(Currency.unwrap(key.currency0)).transferFrom(sender, address(poolManager), amount0);
+            poolManager.settle();
+            poolManager.mint(address(this), key.currency0.toId(), amount0);
+        }
+        if (amount1 > 0) {
+            poolManager.sync(key.currency1);
+            IERC20Minimal(Currency.unwrap(key.currency1)).transferFrom(sender, address(poolManager), amount1);
+
+            poolManager.settle();
+            poolManager.mint(address(this), key.currency1.toId(), amount1);
+        }
+        return "";
     }
 }
